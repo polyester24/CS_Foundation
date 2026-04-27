52a41d81b7a6502d942bc5a8c2b1343a1c859a60
poly <aoi.itabashi@softage.co.jp>
2026-04-27 15:45:15 +0900

    finished commit

--- Example/Program.cs
+++ Example/Program.cs
@@ -4,6 +4,10 @@ static class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+     Console.Write("文字列を入力してください->");
+
+    //変数sにユーザーが入力した値を格納し表示
+    string s = Console.ReadLine();
+    Console.WriteLine(s);
     }
 }

febaf3501a46f52c97840e99bf37b3201eda6300
poly <aoi.itabashi@softage.co.jp>
2026-04-28 10:55:59 +0900

    finished5~7 commit

---
+++ Ex_02_05/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Ex_02_05;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        var num = Console.ReadLine();
+        int number;
+        number = int.Parse(num);
+        int result;
+
+        if(number % 2 == 1)
+        {
+            result =  number;
+            Console.WriteLine($"結果:{result}");
+        }
+        else
+        {
+            result = number * 10;
+            Console.WriteLine($"結果:{result}");
+        }
+    }
+}
---
+++ Ex_02_06/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Ex_02_06;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("数値を入力してください->");
+        var num = Console.ReadLine();
+        int number;
+        number = int.Parse(num);
+
+        if(number % 4 == 0)
+        {
+            Console.WriteLine("4の倍数です。");
+        }
+        else
+        {
+            Console.WriteLine("4の倍数ではありません。");
+        }
+    }
+}
